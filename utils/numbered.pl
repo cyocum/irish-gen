773657af244350a53c88c85fc03ee083406b2578
Chris Yocum <cyocum@gmail.com>
2017-10-28 10:00:39 +0100

    Added genelach_ceníuil_bóguine.ttl
    * Added genelach_ceníuil_bóguine.ttl
    * added numbers to the numbered.pl utility

--- utils/numbered.pl
+++ utils/numbered.pl
@@ -10,7 +10,8 @@ my %num = (
     "Sé" => 6,
     "Secht" => 7,
     "Ocht" => 8,
-    "Noí" => 9
+    "Noí" => 9,
+    "Deich" => 10
     );
 
 my $str = $ARGV[0];
@@ -21,14 +22,15 @@ my ($numChild, $person) = split /\sm\s|\smc\s|\s\.?m\.?\s|\smeic\s/, $head;
 
 my $person_no_space = $person =~ s/\s+//gr;
 
-my $output = "<#$person>\n a foaf:Person;\nirishRel:genName \"$person\";\nirishRel:numChild " . $num{$numChild} . ".\n\n";
+my $output = "<#$person_no_space>\n a foaf:Person;\nirishRel:genName \"$person\";\nirishRel:numChild " . $num{$numChild} . ".\n\n";
 
 my @children = split /\set\s|&|,|\./, $tail;
 
 foreach my $child (@children) {
     $child =~ s/^\s+|\s+$//g;
+    my $child_no_space = $child =~ s/\s+//gr;
     my $person_space_removed = $person =~ s/\s+//gr;
-    $output .= "<#$child>\n a foaf:Person;\nirishRel:nomName \"$child\";\nrel:childOf <#$person_space_removed>.\n\n";
+    $output .= "<#$child_no_space>\n a foaf:Person;\nirishRel:nomName \"$child\";\nrel:childOf <#$person_space_removed>.\n\n";
 }
 
 print $output;
