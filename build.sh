977da32860c32e2dba97574bd16f4a0bac091cf9
Chris Yocum <cyocum@gmail.com>
2018-11-04 17:15:08 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt update
-apt install -y wget
+apt install -y wget openjdk-11-jdk-headless
 wget https://www-eu.apache.org/dist/jena/binaries/apache-jena-3.9.0.tar.gz
 tar xfvz apache-jena-3.9.0.tar.gz
 find . -name "*.trig" -print0 | xargs -0 ./apache-jena-3.9.0/bin/riot --validate --verbose
