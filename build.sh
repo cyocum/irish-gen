f3d24d2e94baaec33fb1bd203fcd3cc2dd2b44b4
Chris Yocum <cyocum@gmail.com>
2018-11-04 17:27:02 +0000

    Added sed

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ apt update
 apt install -y wget openjdk-11-jdk-headless
 wget https://www-eu.apache.org/dist/jena/binaries/apache-jena-3.9.0.tar.gz
 tar xfvz apache-jena-3.9.0.tar.gz
+sed -i 's/WARN/INFO/g' ./apache-jena-3.9.0/jena-log4j.properties
 find . -name "*.trig" -print0 | xargs -0 ./apache-jena-3.9.0/bin/riot --validate --verbose
 
