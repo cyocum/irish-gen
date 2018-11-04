e52fe6ab24fad7ac74f25142eb9a85501c7095a3
Chris Yocum <cyocum@gmail.com>
2018-11-04 17:12:32 +0000

    Removed things and added them to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+apt update
+apt install -y wget
 wget https://www-eu.apache.org/dist/jena/binaries/apache-jena-3.9.0.tar.gz
 tar xfvz apache-jena-3.9.0.tar.gz
 find . -name "*.trig" -print0 | xargs -0 ./apache-jena-3.9.0/bin/riot --validate --verbose
-echo "WTF"
+
