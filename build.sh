c8efdd65830d00b8c80495370ea155d075a86378
Chris Yocum <cyocum@gmail.com>
2019-02-23 19:34:39 +0000

    Fixed more build issues

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -name '*.trig' -print0 | xargs -0 /apache-jena-3.9.0/bin/riot --validate --verbose
+find / -name '*.trig' -print0 | xargs -0 /apache-jena-3.10.0/bin/riot --validate --verbose
