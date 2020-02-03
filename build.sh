a249d06f773eb386f038c5eaf84b56ba15f0156a
Christopher Yocum <cyocum@gmail.com>
2020-02-03 19:13:15 +0000

    Updating Apache Jena version for syntax check

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -name '*.trig' -print0 | xargs -0 /apache-jena-3.13.1/bin/riot --validate --verbose
+find / -name '*.trig' -print0 | xargs -0 /apache-jena-3.14.0/bin/riot --validate --verbose
