8393a47723582d260022dfb61c583d88e884f0d3
Christopher Yocum <cyocum@gmail.com>
2020-04-11 21:09:35 +0100

    Ensure that check file syntax does not return 1 on success

--- utils/check_file_syntax.sh
+++ utils/check_file_syntax.sh
@@ -7,3 +7,13 @@ then
 fi
 
 find . -name '*.trig' -print0 | xargs -0 $JENA_HOME/bin/riot --validate --verbose | egrep -B 1 "WARN|ERROR"
+
+# if grep does not match something, it will set $? to 1 but
+# in our case if it does not find an ERROR or WARN then that
+# means success so flip the usual idea of failure in this case
+if [ $? -eq 1 ]
+then
+    exit 0
+else
+    exit 1
+fi
