3dbe8c525bddd54b38780dee1830ad76b2a60450
Christopher Yocum <cyocum@gmail.com>
2022-07-03 17:06:55 +0100

    Added agrleon to NLS.Adv.72.1.1/Highland_Pedigrees/kindred_34.trig
    * Added agrleon to NLS.Adv.72.1.1/Highland_Pedigrees/kindred_34.trig
    * Removed dead code in utils/check_file_syntax.sh

--- utils/check_file_syntax.sh
+++ utils/check_file_syntax.sh
@@ -14,8 +14,6 @@ fi
 
 find $IRISH_GEN_HOME  \( -name '*.trig' -o -name '*.ttl' \) -exec $JENA_HOME/bin/riot --validate --verbose 2>&1 {} + | egrep -B 1 "WARN|ERROR"
 
-# find $IRISH_GEN_HOME  \( -name '*.trig' -o -name '*.ttl' \) -type f -print0 | xargs -0 $JENA_HOME/bin/riot --validate --verbose 2>&1 | egrep -B 1 "WARN|ERROR"
-
 # if grep does not match something, it will set $? to 1 but
 # in our case if it does not find an ERROR or WARN then that
 # means success so flip the usual idea of failure in this case
